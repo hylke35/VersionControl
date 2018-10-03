fab09ee9875322897a7205cd838263b3f4759b9b
sytzewest <43606602+sytzewest@users.noreply.github.com>
2018-10-03 15:02:12 +0200

    Added form to php
    Initial form added to php file.

--- Fortnite.php
+++ Fortnite.php
@@ -4,7 +4,13 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-    </head
+    </head>
+    
+    <form>
+        <input type="text" name="email" placeholder="Email"><br>
+        <input type="password" name="password" placeholder="Password"><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
     
     <?php
         
